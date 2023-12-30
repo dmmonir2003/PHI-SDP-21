7a7af5f963735c0f648f3e59e9dddf177015fe31
dmmonir2003 <dmmonir2003@gmail.com>
2023-12-31 01:46:47 +0600

    Adding Styles from backend,

--- accounts/forms.py
+++ accounts/forms.py
@@ -52,3 +52,16 @@ class UserForm(UserCreationForm):
 
         return our_user
     
+    # style add from bacend
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({
+                'class':(
+                    'appearance-none block w-full bg-gray-200 ' 
+                    'text-gray-700 border border-gray-200 rounded '
+                    'py-3 px-4 leading-tight focus:outline-none '
+                    'focus:bg-white focus:border-gray-500'
+                )
+            })

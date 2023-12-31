09ebd18de8d0acf81fba30d87af7fb9093e3b751
dmmonir2003 <dmmonir2003@gmail.com>
2023-12-31 19:11:09 +0600

    Creating Base HTML

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from .views import UserRegistrationView
 urlpatterns = [
     
-    path('register/',UserRegistrationView.as_view(),name='register' ),
-    # path('home/',UserRegistrationView.as_view(),name='home' ),
+    path('register/',UserRegistrationView.as_view(),name='register'),
+    path('login/',UserRegistrationView.as_view(),name='login' ),
+    path('logout/',UserRegistrationView.as_view(),name='logout' ),
+    path('profile/',UserRegistrationView.as_view(),name='profile' ),
+   
 ]

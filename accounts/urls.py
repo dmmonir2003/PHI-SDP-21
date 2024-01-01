cc0544d1951ae02e431b794693ee131e45f05e59
dmmonir2003 <dmmonir2003@gmail.com>
2024-01-02 03:17:59 +0600

    implement profile update

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,11 @@
 
 from django.urls import path
-from .views import UserRegistrationView,UserLoginView,UserLogoutView
+from .views import UserRegistrationView,UserLoginView,UserLogoutView,UserProfileUpdate
 urlpatterns = [
     
     path('register/',UserRegistrationView.as_view(),name='register'),
     path('login/',UserLoginView.as_view(),name='login' ),
     path('logout/',UserLogoutView.as_view(),name='logout' ),
-    path('profile/',UserRegistrationView.as_view(),name='profile' ),
+    path('profile/',UserProfileUpdate.as_view(),name='profile' ),
    
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from django.views.generic import FormView
-from .forms import UserForm
+from django.shortcuts import render,redirect
+from django.views.generic import FormView,UpdateView
+# from django.views.generic.edit import UpdateView
+
+from .forms import UserForm,UserUpdateForm
 from django.contrib.auth import login,logout
 from django.contrib.auth.views import LoginView,LogoutView
 from django.urls import reverse_lazy
@@ -32,3 +34,24 @@ class UserLogoutView(LogoutView):
          if self.request.user.is_authenticated:
              logout(self.request)
          return  reverse_lazy('home')
+
+class UserProfileUpdate(UpdateView):
+    template_name='accounts/profile.html'
+    form_class=UserUpdateForm
+    success_url=reverse_lazy('home')
+    def get_object(self, queryset=None):
+        return self.request.user
+    
+# class UserProfileUpdate(View):
+#     template_name='accounts/profile.html'
+    
+
+#     def get(self,request):
+#         form=UserUpdateForm(instance=request.user)
+#         return render(request,self.template_name,{'form':form})
+#     def post(self,request):
+#         form=UserUpdateForm(request.POST,instance=request.user)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('home')
+#         return render(request,self.template_name,{'form':form})

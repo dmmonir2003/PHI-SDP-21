51f2a5058de4aaa25897cced5e893ac499767ab3
dmmonir2003 <dmmonir2003@gmail.com>
2024-01-02 16:19:53 +0600

    Creatig Deposit, withdrawal and loan form

--- transactions/forms.py
+++ transactions/forms.py
@@ -22,3 +22,40 @@ class TransactionForm(forms.ModelForm):
           self.instance.balance_after_transaction=self.account.balance
           return super().save()
           
+class DepositForm(TransactionForm):
+     def clean_amount(self):
+          min_deposit_amount=100
+          amount=self.cleaned_data.get('amount')
+          if amount<min_deposit_amount:
+               raise forms.ValidationError(
+                    f'tou need to deposit at least {min_deposit_amount} Taka'
+               )
+          return amount
+     
+class WithdrawForm(TransactionForm):
+     def clean_amount(self):
+          account=self.account
+          min_withdraw_amount=500
+          max_withdraw_amount=50000
+          balance=account.balance
+          amount=self.cleaned_data.get('amount')
+          if amount<min_withdraw_amount:
+               raise forms.ValidationError(
+                    f'You can withdraw at least {min_withdraw_amount}  Taka'
+               )
+          if amount>max_withdraw_amount:
+               raise forms.ValidationError(
+                    f'You can withdraw at most {max_withdraw_amount} Taka'
+               )
+          if amount>balance:
+               raise forms.ValidationError(
+                    f' You have {balance} Taka in your account '
+                    'You Can not withdraw more then your account current balance'
+               )
+          return amount
+     
+class LoanRequestForm(TransactionForm):
+     def clean_amount(self):
+          amount=self.cleaned_data.get('amount')
+
+          return amount

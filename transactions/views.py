c8b70ec37f19cb0634ff0228688c4e6eaacb53df
dmmonir2003 <dmmonir2003@gmail.com>
2024-01-03 16:46:22 +0600

    Creating Withdraw and Loan request view

--- transactions/views.py
+++ transactions/views.py
@@ -8,7 +8,7 @@ from .models import Transaction
 from .constans import DEPOSIT,WITHDRAWAL,LOAN,LOAN_PAID
 from .forms import DepositForm,WithdrawForm,LoanRequestForm
 from django.contrib import messages
-
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -51,6 +51,39 @@ class DepositView(TransactionCreateViewMixin):
             update_fields=['balance']
         )
         messages.success(self.request,f'Your amount {amount} add to your account ')
+
+        return super().form_valid(form)
+class WithdrawView(TransactionCreateViewMixin):
+    form_class=WithdrawForm
+    title='Withdraw'
+
+    def get_initial(self):
+        initial={'transaction_type':WITHDRAWAL}
+        return initial
+    def form_valid(self, form):
+        amount=form.cleaned_data.get('amount')
+        account=self.request.user.account
+        account.balance-=amount
+        account.save(
+            update_fields=['balance']
+        )
+        messages.success(self.request,f'Your amount {amount} withdraw to your account ')
+
+        return super().form_valid(form)
+class LoanRequestView(TransactionCreateViewMixin):
+    form_class=LoanRequestForm
+    title='LoanRequest'
+
+    def get_initial(self):
+        initial={'transaction_type':LOAN}
+        return initial
+    def form_valid(self, form):
+        amount=form.cleaned_data.get('amount')
+        current_loan_count=Transaction.objects.filter(account=self.request.user.account,transaction_type=LOAN,loan_approve=True).count()
+        if current_loan_count>=3:
+            return HttpResponse('Your loan limits already crossed ')
+        
+        messages.success(self.request,f'Your amount {amount} Loan request send  to admin successfully  ')
 
         return super().form_valid(form)
     
